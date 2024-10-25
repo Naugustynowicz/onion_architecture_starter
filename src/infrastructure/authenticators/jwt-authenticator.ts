93609483d252258405c3efd22c367a2ce266259d
naugustynowicz <nicolas.augustynowicz@gmail.com>
2024-10-25 16:54:39 +0200

    basic jwt authentificator

--- src/infrastructure/authenticators/jwt-authenticator.ts
+++ src/infrastructure/authenticators/jwt-authenticator.ts
@@ -1,5 +1,11 @@
-import { IAuthenticator } from "../../interfaces/authenticator.interface"
-import { IUserRepository } from "../../interfaces/user-repository.interface"
+import { jwtDecode } from "jwt-decode";
+import { IAuthenticator } from "../../interfaces/authenticator.interface";
+import { IUserRepository } from "../../interfaces/user-repository.interface";
+
+type CustomJwtPayload = {
+    email: string,
+    password: string
+}
 
 export class JwtAuthenticator implements IAuthenticator{
     constructor(
@@ -7,6 +13,14 @@ export class JwtAuthenticator implements IAuthenticator{
     ){}
 
     async authenticate(token: string){
-        throw new Error('Not implemented.')
+        // const decoded =  Buffer.from(token, 'base64').toString('utf-8')
+        const decoded = jwtDecode<CustomJwtPayload>(token);
+
+        const [email, password] = [decoded.email, decoded.password]
+        const user = await this.userRepository.findByEmail(email)
+
+        if(!user || user.props.password !== password) throw new Error("Wrong credentials.")
+
+        return user
     }
 }
--- src/tests/unit/jwt-authenticator.test.ts
+++ src/tests/unit/jwt-authenticator.test.ts
@@ -14,7 +14,7 @@ describe('Authenticator', () => {
     })
 
     describe('Scenario: token is valid', () => {
-        const payload = Buffer.from(`${testUsers.johnDoe.props.email}:${testUsers.johnDoe.props.password}`).toString('base64')
+        const payload = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG5kb2VAZ21haWwuY29tIiwicGFzc3dvcmQiOiJxd2VydHkifQ.0fz20ShOf1wmIYS6UqUd2mkjUaQp5NudAfi3hsHT53U'
 
         it('should authenticate a user', async () => {
             const user = await authenticator.authenticate(payload)
@@ -28,7 +28,7 @@ describe('Authenticator', () => {
     })
     
     describe('Scenario: token is not valid', () => {
-        const payload = Buffer.from(`unknow@gmail.com:${testUsers.johnDoe.props.password}`).toString('base64')
+        const payload = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVudmFsaWRFbWFpbCIsInBhc3N3b3JkIjoicXdlcnR5In0.0hRWuuKBOqGJeXv1LwiIiOHT4apJ2V5vggXEj3JgSEc'
 
         it('should throw an error', async () => {
             await expect(authenticator.authenticate(payload)).rejects.toThrow('Wrong credentials.')
@@ -36,7 +36,7 @@ describe('Authenticator', () => {
     })
 
     describe('Scenario: password is not valid', () => {
-        const payload = Buffer.from(`${testUsers.johnDoe.props.email}:unvalidPassword`).toString('base64')
+        const payload = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG5kb2VAZ21haWwuY29tIiwicGFzc3dvcmQiOiJ1bnZhbGlkUGFzc3dvcmQifQ.pT4rQe8OP5CK3nNenuk5xbrJFQ0PygIxnQm9OgWAWzY'
 
         it('should throw an error', async () => {
             await expect(authenticator.authenticate(payload)).rejects.toThrow('Wrong credentials.')
